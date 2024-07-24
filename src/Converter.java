078be6c1f7a6dc45109e327d719a08505d148f07
Izotov Valentin <devices111@yandex.ru>
2024-07-24 22:20:23 +0300

    Переименовал final константы в UPPER_SNAKE_CASE

--- src/Converter.java
+++ src/Converter.java
@@ -1,18 +1,15 @@
 public class Converter {
-
     //Переводим количество шагов в километры
-    int convertToKm(int steps) {
-        final int centimetersPerStep = 75;
-        final int centimetersPerKilometer = 100000;
-        return (steps * centimetersPerStep / centimetersPerKilometer);
+    static int convertToKm(int steps) {
+        final int CENTIMETERS_PER_STEP = 75;
+        final int CENTIMETERS_PER_KILOMETER = 100000;
+        return (steps * CENTIMETERS_PER_STEP / CENTIMETERS_PER_KILOMETER);
     }
 
     //Переводим количество шагов в килокалории
-    int convertStepsToKilocalories(int steps) {
-        final int caloriesPerStep = 50;
-        final int caloriesPerKilocalorie = 1000;
-        return (steps * caloriesPerStep / caloriesPerKilocalorie);
+    static int convertStepsToKilocalories(int steps) {
+        final int CALORIES_PER_STEP = 50;
+        final int CALORIES_PER_KILOCALORIE = 1000;
+        return (steps * CALORIES_PER_STEP / CALORIES_PER_KILOCALORIE);
     }
-
-
 }
